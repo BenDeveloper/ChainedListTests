8e2a1ec42cb892ed9703b92970eeed6c01dfc321
Ben <lavaud.benoit@hotmail.com>
2020-11-30 16:01:31 +0100

    Program tests

--- ChainedList/Program.cs
+++ ChainedList/Program.cs
@@ -6,7 +6,43 @@ namespace ChainedList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("ChainedList Tests");
+
+            // Création liste
+            ChainedList<int> chainedList = new ChainedList<int>(10);
+
+            // Ajout 5, 10, 4
+            chainedList.Add(5, 10, 4);
+
+            // Afficher
+            Console.WriteLine("Nb element : " + chainedList.ElementCount);
+            Console.WriteLine("Size table : " + chainedList.tableSize);
+
+            Console.WriteLine("first : " + chainedList.FirstElement.Value);
+            Console.WriteLine("last : " + chainedList.LastElement);
+
+            Console.WriteLine("***************");
+
+            // Add 99 1st position
+            chainedList.AddAt(99, 0);
+
+            for (int i=0; i < chainedList.ElementCount; i++)
+            {
+                Console.WriteLine(chainedList.GetElementByKey(i));
+            }
+            Console.WriteLine("***************");
+
+            // Add 33 2nd position
+            chainedList.AddAt(33, 1);
+
+            Console.WriteLine(chainedList.GetElementByKey(0));
+            Console.WriteLine(chainedList.GetElementByKey(1));
+            Console.WriteLine(chainedList.GetElementByKey(2));
+            Console.WriteLine(chainedList.GetElementByKey(3));
+            Console.WriteLine(chainedList.GetElementByKey(4));
+            Console.WriteLine("***************");
+
+            Console.ReadLine();
         }
     }
 }
