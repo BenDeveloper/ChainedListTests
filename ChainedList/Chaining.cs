eec1af90ef94f06826f15e5bb63b4f50bbf5506b
Ben <lavaud.benoit@hotmail.com>
2020-11-30 16:00:48 +0100

    Chaining generic class

---
+++ ChainedList/Chaining.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainedList
+{
+    public class Chaining<T>
+    {
+        public T Value { get; set; }
+        public Chaining<T> Previous { get; set; }
+        public Chaining<T> Next { get; set; }
+    }
+}
